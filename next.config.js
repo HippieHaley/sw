bdc22bd10ac3a523075da7fefc437687c6987bf4
HippieHaley <haley.m.patino@gmail.com>
2025-11-18 05:41:37 +0000

    Disable output file tracing to fix build

--- next.config.js
+++ next.config.js
@@ -2,19 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    outputFileTracingExcludes: {
-      '*': [
-        'node_modules/@swc/core-linux-x64-gnu',
-        'node_modules/@swc/core-linux-x64-musl',
-        'node_modules/@esbuild',
-        'node_modules/webpack',
-        'node_modules/rollup',
-        'node_modules/terser',
-      ],
-    },
-  },
-  outputFileTracing: true,
+  output: 'standalone',
+  outputFileTracing: false,
   headers: async () => {
     return [
       {
